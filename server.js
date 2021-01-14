570083a07052b2f78b36cc4943e8da0e4b2a60f9
Eihab <mastereihab@gmail.com>
2021-01-14 20:19:53 +0100

    added compression library for gzipping on heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const cors = require("cors") //Cross origin requests; making requests on multiple ports
 const bodyParser = require("body-parser")
 const path = require("path") // Allow us to build pathing for our directories 
+const compression = require('compression')
 
 if (process.env.NODE_ENV !== 'production') require("dotenv").config()
 
@@ -10,12 +11,12 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const app = express()
 const port = process.env.PORT || 8000
 
+app.use(compression())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(cors())
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {    
     app.use(express.static(path.join(__dirname, 'client/build')))
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
